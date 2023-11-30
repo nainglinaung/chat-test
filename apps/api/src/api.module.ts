6473252e6f76e8a534a1721f1bee2ca4b54f11c8
nainglinaung <nainglinaung91@gmail.com>
2023-11-30 19:41:33 +0630

    add config

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
-
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+// import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [ConfigModule.forRoot()],
   controllers: [ApiController],
   providers: [ApiService],
 })
