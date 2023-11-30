7af8997abbbff2c0c14481fd7ac9f9cb3313d47e
nainglinaung <nainglinaung91@gmail.com>
2023-11-30 13:02:34 +0630

    auth implementation

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,10 +1,24 @@
 import { PrismaService } from '@app/common/prisma/prisma.service';
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async createUser(data) {
+    const saltOrRounds = 10;
+    data.password = await bcrypt.hash(data.password, saltOrRounds);
+    const user = await this.prismaService.user.create({ data });
+    const payload = { sub: user.id };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
 
   async login(data) {
     const user = await this.prismaService.user.findFirst({
